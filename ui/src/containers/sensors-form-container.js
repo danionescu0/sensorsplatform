b3e260f12a8a4b49be9aa7bb5020220f834766df
Cosmin Seceleanu <coosmin26@yahoo.com>
2017-09-21 21:23:25 +0300

    add map to select sensor position on form

--- ui/src/containers/sensors-form-container.js
+++ ui/src/containers/sensors-form-container.js
@@ -5,13 +5,26 @@ import SensorsForm from '../components/sensors/sensors-form';
 import withForm from '../hoc/form';
 import {postJson} from "../utils/fetch";
 import Auth from "../utils/auth";
+import AdminContent from '../components/admin-content';
 
 class SensorsFormContainer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            errorMessage: null
+            errorMessage: null,
+            location: {
+                lat: null,
+                lng: null,
+            },
         }
+    }
+
+    onLocationChanged(e) {
+        const lat = e.latLng.lat();
+        const lng = e.latLng.lng();
+        this.setState({
+            location: {lat: lat, lng: lng}
+        });
     }
 
     handleSubmit(e) {
@@ -23,9 +36,17 @@ class SensorsFormContainer extends Component {
             });
             return;
         }
-        postJson(`/user-sensors/${Auth.getUserId()}`, form).then(() => {
+        if (!this.state.location.lng || !this.state.location.lng) {
+            this.setState({
+                errorMessage: "Please select sensor location on map"
+            });
+            return;
+        }
+        const sensor = Object.assign({}, form, this.state.location);
+
+        postJson(`/user-sensors/${Auth.getUserId()}`, sensor).then(() => {
             this.setState({errorMessage: ""});
-            this.props.onAddSuccess();
+            this.props.history.push("/sensors");
         }).catch(e => {
             this.setState({errorMessage: "Request failed"});
         });
@@ -35,13 +56,15 @@ class SensorsFormContainer extends Component {
         const {handleInputChange} = this.props;
 
         return (
-            <SensorsForm errorMessage={this.state.errorMessage}
-                 handleInputChange={handleInputChange}
-                 handleSubmit={this.handleSubmit.bind(this)}
-                 toogleModal={this.props.toogleModal}
-                 isModalOpen={this.props.isModalOpen}
-                 error={this.state.errorMessage}
-            />
+            <AdminContent>
+                <SensorsForm errorMessage={this.state.errorMessage}
+                             handleInputChange={handleInputChange}
+                             handleSubmit={this.handleSubmit.bind(this)}
+                             error={this.state.errorMessage}
+                             onLocationChanged={this.onLocationChanged.bind(this)}
+                             location={this.state.location}
+                />
+            </AdminContent>
         )
     }
 }
