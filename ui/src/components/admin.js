d6f363dcb2069ccd2b881e65cc399500954d9ed3
Cosmin Seceleanu <8344031+cosminseceleanu@users.noreply.github.com>
2017-09-21 21:51:11 +0300

    add map with sensors positions

--- ui/src/components/admin.js
+++ ui/src/components/admin.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    Col
+} from 'ahoy-reactstrap';
 
 import AdminContent from './admin-content';
+import MapWithMarkers from './map-with-markers';
 
 const Admin = () => {
+
+    const markers = [
+        {position: { lat: 44.425908, lng: 26.1236888 }},
+        {position: { lat: 44.446686, lng: 26.0346153 }},
+        {position: { lat: 44.4280871, lng: 26.1271264}},
+        {position: { lat: 44.4161778, lng: 26.1509529}},
+    ];
+
     return (
         <AdminContent>
-            <div>Dashboard</div>
+            <Col lg="12">
+                <Card className="mb-5">
+                    <CardHeader>Sensors positions</CardHeader>
+                    <CardBody>
+                        <MapWithMarkers markers={markers}/>
+                    </CardBody>
+                </Card>
+            </Col>
         </AdminContent>
     )
 };
