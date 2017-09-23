4c5acb9aba6036c89c0f6e0b8541533bf809932e
Dan Ionescu <nitzu00@yahoo.com>
2017-09-23 21:09:15 +0300

    fix for rabbit mq disconnecting

--- backend/services/AsyncJobs.py
+++ backend/services/AsyncJobs.py
@@ -11,11 +11,18 @@ class AsyncJobs:
 
     def register_event(self, event_name: str):
         self.__event_name = event_name
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host = self.__host, port=5672))
-        self.__channel = connection.channel()
-        self.__channel.exchange_declare(exchange=event_name, exchange_type='fanout')
+        self.__do_register_event()
+
+    def __do_register_event(self):
+        self.__connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host=self.__host, port=5672, heartbeat_interval=120)
+        )
+        self.__channel = self.__connection.channel()
+        self.__channel.exchange_declare(exchange=self.__event_name, exchange_type='fanout')
 
     def publish(self, event: Event) -> None:
+        if not self.__connection.is_open:
+            self.__do_register_event()
         self.__channel.basic_publish(exchange=event.name, routing_key ='', body = pickle.dumps(event))
 
     def consume(self, callback) -> None:
