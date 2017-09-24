f437d76f1405468b785a73f484637500965371a3
Dan Ionescu <nitzu00@yahoo.com>
2017-09-24 20:05:50 +0300

    begin implementation for long term data storage, splitted tasks in momentary data and durable data

---
+++ backend/model/DataPoint.py
@@ -0,0 +1,12 @@
+import uuid
+from cassandra.cqlengine import columns
+from cassandra.cqlengine import connection
+from datetime import datetime
+from cassandra.cqlengine.management import sync_table
+from cassandra.cqlengine.models import Model
+
+class DataPoint:
+    def __init__(self, sensor_id: str, timestamp: int, value):
+        self.sensor_id = sensor_id
+        self.timestamp = timestamp
+        self.value = value
